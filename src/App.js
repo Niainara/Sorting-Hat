38b1719a341e8e521675c40d85ceccda10438bb0
Anja Razafitrimo <anjazarafitrimo@msn.com>
2019-02-13 10:55:22 +0100

    app terminée

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Provider } from  'react-redux';
+import { createStore } from  'redux';
+import Result from './Containers/Result';
+let selectedHouses = '';
+const houses = [
+	'Serpentard',
+	'Griffondor',
+	'Serdaigle',
+	'Pouffsoufle',
+];
+function reducer(state = {houses, selectedHouses}, action) {
+	console.log('reducer',state.houses, state.selectedHouses,action)
+	switch (action.type) {
+		case 'UPDATE_CHOICE_WITH_DELAY':
+			return {
+				houses,
+				selectedHouses:  houses[action.indexSelected]
+			}; 
+		default:
+			return state; 
+	}
+}
+const  store = createStore(reducer);
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+			<Provider  store={store}>
+			<Result  />
+			</Provider>
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/Containers/Result.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Message from '../Components/Message';
+
+class Result extends React.Component {
+	constructor(props){
+		super(props);
+		const action = {
+			type: 'UPDATE_CHOICE_WITH_DELAY',
+			indexSelected: Math.floor(Math.random()  *  4)
+		}
+		setInterval(() =>  this.props.dispatch(action), 1000)
+	}
+	render() {
+		console.log(this.props)
+		return (
+			<div>
+			<ul>
+			{this.props.houses.map(house  =>  <li  key={house}>{house}</li>)}
+			</ul>
+			<Message  message={this.props.selectedHouses}  />
+			</div>
+		)
+	}
+}
+export  default  connect(store=>store)(Result)
